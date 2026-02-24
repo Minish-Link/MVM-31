1e62849462d6ee7029afcab8cfe4df0ca76fa958
DwarfWoot <dwarfwoot@gmail.com>
2026-02-24 12:27:26 -0800

    Added Debug Commands
    Added commands to Reset Level (CTRL + R) and Give Items (CTRL + I)

---
+++ objects/player/player_debug.gd
@@ -0,0 +1,19 @@
+extends Node3D
+class_name DebugStuff
+
+func _physics_process(delta: float) -> void:
+	_reset_level_command()
+	_give_item()
+	
+func _reset_level_command():
+	if (Input.is_key_pressed(KEY_F13) and Input.is_joy_button_pressed(0,JOY_BUTTON_BACK)) or (
+		(Input.is_key_pressed(KEY_CTRL) and Input.is_key_pressed(KEY_R))):
+		print("Reset Level Command Activated")
+		print(get_tree().current_scene)
+		print("Reload Outpint: %s" %get_tree().reload_current_scene())
+		get_tree().reload_current_scene()
+		PlayerCharacter.position = Vector3(0,0,0)
+	
+func _give_item():
+	if (Input.is_key_pressed(KEY_CTRL) and Input.is_key_pressed(KEY_I)):
+		WorldData.collect_item("Slide")
